c87f3b33f237b394bd3b1ad49c0e53fa875d7dec
Misha <mymisha1988@gmail.com>
2025-02-12 22:07:07 +0800

    Updates

---
+++ precache-manifest.532fa8f4c9875efaa986df90c83d402f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f69578e89987245db6c6a57815c9a2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f85ca26e5c41a225b66f",
+    "url": "/portfolio/static/css/main.faa731d0.chunk.css"
+  },
+  {
+    "revision": "855567225f82670e631e",
+    "url": "/portfolio/static/js/2.81e0801a.chunk.js"
+  },
+  {
+    "revision": "1299ed581ef47dcd8388bb4526f15704",
+    "url": "/portfolio/static/js/2.81e0801a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5ec699c37e001452f4b",
+    "url": "/portfolio/static/js/3.eda73589.chunk.js"
+  },
+  {
+    "revision": "f85ca26e5c41a225b66f",
+    "url": "/portfolio/static/js/main.20ddaa3d.chunk.js"
+  },
+  {
+    "revision": "1234de98d05833e70212",
+    "url": "/portfolio/static/js/runtime-main.4c5176b2.js"
+  },
+  {
+    "revision": "54b331750a1fccc36def7f942f65d31e",
+    "url": "/portfolio/static/media/Jersey15-Regular.54b33175.ttf"
+  },
+  {
+    "revision": "15e5b19fa37c36e98e15fd7588635107",
+    "url": "/portfolio/static/media/banner.15e5b19f.png"
+  },
+  {
+    "revision": "8034349139c3c40870921ec5ae60d5f4",
+    "url": "/portfolio/static/media/gatcha-closed.80343491.png"
+  },
+  {
+    "revision": "025bf2f3de4f2b1506c37e41a31d84e1",
+    "url": "/portfolio/static/media/gatcha-opened.025bf2f3.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "7d12ee4e6fd3c1c6624cf270a8a09ba0",
+    "url": "/portfolio/static/media/me.7d12ee4e.png"
+  },
+  {
+    "revision": "13e5b63ab62c279fc0d44e669efacdc6",
+    "url": "/portfolio/static/media/vending-machine1.13e5b63a.png"
+  }
+]);
